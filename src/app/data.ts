cb8b4907ea76d3b670ac9489ec9d04a925391d45
Darel Honrejas <133852063+D4rzDev@users.noreply.github.com>
2025-02-11 13:46:15 +0800

    pooosh

--- src/app/data.ts
+++ src/app/data.ts
@@ -22,3 +22,246 @@ export const faq = [
     {question: 'How can i invite friends to play Hashtag?', answer:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta culpa quos repudiandae, expedita quas fugit nisi blanditiis, est dolorem a molestiae vel similique temporibus exercitationem doloribus placeat ab qui! Sed?'},
 ]
 
+export const termsAndConditions = [
+    {
+      title: "Introduction",
+      content:
+        "Welcome to Hashtag! These Terms and Conditions outline how you can use our platform, Hashtag (referred to as the 'Service'). By accessing or using our Service, you agree to follow these Terms. If you don't agree, we kindly ask that you refrain from using our Service. Thanks for understanding, and happy hashtagging!",
+    },
+    {
+      title: "Acceptance of Terms",
+      content:
+        "By utilizing our Service, you confirm that you are at least 18 years old and possess the legal capacity to agree to these Terms. If you are accessing our Service on behalf of an organization or entity, you affirm that you have the necessary authority to bind that entity to these Terms.",
+    },
+    {
+      title: "Description of Service",
+      content:
+        "Hashtag provides an automatic share market trading robot ('Hash Bot') that executes trades on your behalf based on predefined algorithms and strategies. The Service is provided on a subscription basis, and we may offer different subscription plans with varying features.",
+    },
+    {
+      title: "Account Registration",
+      content:
+        "To use our Service, you must create an account by providing accurate and complete information. You are responsible for maintaining the confidentiality of your account credentials and for all activities that occur under your account. You agree to notify us immediately of any unauthorized use of your account.",
+    },
+    {
+      title: "Subscription and Fees",
+      content:
+        `a. Subscription Plans: We offer various subscription plans for accessing our Service, details of the subscription plans, including pricing and features, are available on our system.\n\nb. Payment: By subscribing to our Service, you agree to pay the applicable subscription fees. Payments are due in advance and are non-refundable, except as required by law. We may change our subscription plans and fees at any time, and we will provide you with notice of any changes.`,
+    },
+    {
+      title: "Use of the Service",
+      content:
+        "a. Compliance: You agree to use our Service in compliance with all applicable laws, regulations, and these Terms.\n\nb. Restrictions: You shall not:\n      - Use the Service for any unlawful or fraudulent purpose.\n      - Interfere with or disrupt the Service or servers.\n      - Attempt to gain unauthorized access to any part of the Service.\n      - Use the Service to engage in any trading activity that manipulates the market.",
+    },
+    {
+      title: "Risks and Disclaimer",
+      content:
+        "a. Trading Risks: Automated trading involves significant risks. You acknowledge that trading in financial markets can result in substantial losses, and you should only trade with funds that you can afford to lose.\n\nb. No Warranty: Our Service is provided 'as is' and 'as available' without any warranties of any kind, either express or implied. We do not warrant that the Service will be uninterrupted, error-free, or free of viruses or other harmful components.",
+    },
+    {
+      title: "Limitation of Liability",
+      content:
+        "To the maximum extent permitted by law, Hashtag and its affiliates, officers, directors, employees, and agents shall not be liable for any indirect, incidental, special, consequential, or punitive damages, including loss of profits, data, or use, arising out of or in connection with your use of the Service.",
+    },
+    {
+      title: "Indemnification",
+      content:
+        "You agree to indemnify and hold harmless Hashtag, its affiliates, and their respective officers, directors, employees, and agents from and against any claims, liabilities, damages, losses, and expenses, including reasonable attorneys' fees, arising out of or in any way connected with your use of the Service or violation of these Terms.",
+    },
+    {
+      title: "Intellectual Property",
+      content:
+        "All content and materials available on our Service, including but not limited to text, graphics, logos, and software, are the property of Hashtag or its licensors and are protected by copyright, trademark, and other intellectual property laws.",
+    },
+    {
+      title: "Modifications to the Service and Terms",
+      content:
+        "We reserve the right to modify or discontinue the Service at any time, with or without notice. We may also modify these Terms from time to time. Any changes will be effective immediately upon posting on our website. Your continued use of the Service after any changes constitutes your acceptance of the new Terms.",
+    },
+    {
+      title: "Governing Law and Dispute Resolution",
+      content:
+        "These terms shall be governed by and construed in accordance with the laws of [Your Country/State], without regard to its conflict of law principles. Any disputes arising out of or in connection with these Terms shall be resolved through binding arbitration in [Your Country/State].",
+    },
+    {
+      title: "Contact Us",
+      content:
+        "If you have any questions about these Terms, please contact us at:\n\n      Email: hashtag0112@gmail.com",
+    },
+  ];
+  
+
+  export const privacyPolicy = [
+    {
+      title: "1. Introduction",
+      content: `Hashtag values the privacy of our users. Therefore, this privacy policy explains in detail how we use and protect the information we collect when you visit our website.
+  
+      Read this privacy policy completely. Please refrain from visiting the site if the terms outlined below are not satisfactory to you. We reserve the right to change this policy at any time and will list these changes in the updates section of the policy.
+  
+      By reading this notice and visiting the site, you agree that you understand that customers will not be personally notified when this policy changes.
+  
+      Therefore, we advise our customers to frequently review our privacy policy so that they remain aware of its updates. By using the site, you accept that the posted policy and all its changes apply to your interaction with Hashtag.`,
+    },
+    {
+      title: "2. Information Collected by Hashtag",
+      content: `Personal information may be collected by this site in many ways. This information includes:
+      
+      1. Personal identifying information like your name, address, email, phone number, and other personal data.
+      2. Server data related to the IP address you used to visit our site, which includes your address, browser, OS, access time, and site activity.
+      3. Financial information related to your orders, including your payment method and identifying payment information.
+      4. Social network data, including Facebook permissions and user information from other networks, provided you log onto our site using one of these media sites.
+      5. Mobile device information such as your device ID, model, and location, if you use our site by accessing it through our mobile app.`,
+    },
+    {
+      title: "3. How We Use This Information",
+      content: `Our Hashtag project uses information collected to:
+  
+      • Manage your account information
+      • Customize ads
+      • Deliver promotions
+      • Email your account confirmation
+      • Manage purchases and payments
+      • Increase site efficiency
+      • Notify you of updates
+      • Offer new products
+      • Monitor and prevent theft
+      • Request your customer feedback
+      • Resolve account disputes
+      • Respond to your service requests`,
+    },
+    {
+      title: "4. Information Disclosure",
+      content: `Normally, your information stays on our site. However, below we have listed the situations that may require us to share the information we collect from you:
+      
+      • When required by law, such as for fraud protection
+      • With our third-party providers for payment processing and hosting
+      • With your consent for marketing purposes
+      • When you post comments on the site
+      • To our advertisers, affiliates, and partners
+      • If this site goes bankrupt and data must be transferred`,
+    },
+    {
+      title: "5. Cookies, Trackers, and Online Ads",
+      content: `We may use cookies, trackers, web beacons, and other technology to customize our site and improve your experience. These trackers do not have access to your personal information and can be removed from your browser options.
+  
+      In addition, third-party software provides ads for our site for marketing campaigns. These programs have access to tracking technology to optimize your ads experience.
+  
+      Website analytics such as Google Analytics may also be used to track users and remarket. We do not give these vendors access to your personal information.`,
+    },
+    {
+      title: "6. Third-Party Links",
+      content: `Our website may contain links to third-party websites in the form of policies, ads, and other non-affiliated links.
+  
+      Once you leave our site, we are no longer responsible for how your information is collected and disclosed. Please refer to the privacy policies of those third-party sites for more information.`,
+    },
+    {
+      title: "7. Information Security",
+      content: `We take technical and administrative precautions to protect your data, but we cannot guarantee its safety against all types of fraud or misuse.
+  
+      If you provide personal information, we cannot verify its total security against all types of interception.`,
+    },
+    {
+      title: "8. Do-Not-Track",
+      content: `Some browsers offer Do-Not-Track settings to prevent any information from being distributed.
+  
+      Since these settings have not been legally established as standard practice, we do not acknowledge these settings.`,
+    },
+    {
+      title: "9. Your Rights and Choices",
+      content: `At any time, you may opt to review or change your account settings, including contact information.
+  
+      If you wish to delete your account, you may do so to remove most of your information; however, some identifying information will be retained to prevent fraud.
+  
+      You may also opt out of emails and other correspondences from our site at any time.`,
+    },
+    {
+      title: "10. Regional Privacy Rights",
+      content: `Enumerate your region’s privacy codes and any permits, legal age limits, or associated information necessary for people in your specific jurisdiction.`,
+    },
+    {
+      title: "11. Contact Us",
+      content: `If you have any inquiries or concerns regarding this privacy policy, don't hesitate to get in touch with us at:
+  
+      Email: hashtag0112@gmail.com`,
+    },
+  ];
+  
+
+export const faqs = [
+    {
+      question: "What is Ace that Pays?",
+      answer:
+        "Ace that Pays is a dynamic Play-to-earn action game where players take care of their pets and live with them. The main objective is to feed, bathe and love their pets to earn real-life rewards. Players can also join competitive tournaments, striving to achieve top scores and rise through the leaderboard ranks. With its blend of quick, engaging tapping mechanics and competitive challenges, Ace that Pays offers an exhilarating and lucrative gaming experience.",
+    },
+    {
+      question: "How can I play Ace that Pays?",
+      answer: `
+      <span class='highlight'>Visit the Official Website</span>
+      -Go to <a href="https://www.acethatpays.com" target="_blank">www.acethatpays.com</a> using your preferred web browser. This is the official platform where you can access 
+      the game and begin your journey.
+  
+      <span class='highlight'>Find a Registration Link</span>
+      -Navigate to the social media links provided on the website. These links will direct you to Ace that Pays official social 
+      media pages or communities.
+      -Look for a Team Leader or any active member who can provide you with a valid registration link. This link is 
+      essential for creating your account.
+  
+      <span class='highlight'>Register Your Account</span>
+      -Use the registration link shared with you to sign up on the platform.
+      -Fill in the required details, such as your name, email, and any other necessary information, to complete the 
+      registration process.
+  
+      <span class='highlight'>Request a Payin</span>
+      -After registering, go to your account dashboard and request a Payin. This step is necessary to fund your account.
+      -Wait for the Payin process to be successfully completed. This may take some time depending on the payment 
+      method you choose.
+  
+      <span class='highlight'>Purchase a Pet</span>
+      -Once your account is funded, visit the in-game store to browse the available Pets.
+      -Select a Pet that suits your preferences and budget, then complete the purchase. Each Pet has unique attributes 
+      that can enhance your gameplay experience.
+  
+      <span class='highlight'>Start Playing</span>
+      -After purchasing your Pet, return to your dashboard.
+      -Click on the <strong>Play Now</strong> button to begin playing the game. This will take you to the gameplay interface where you 
+      can interact with your Pet and participate in various activities.
+    `,
+    },
+  
+    {
+      question: "How can I earn money in Ace that Pays?",
+      answer:
+        "In Ace that Pays, taking care of your pets is a fun and interactive way to earn rewards. This involves a variety of activities that ensure your pets are happy, healthy, and well-maintained. For instance, you can feed your pets regularly to keep them energized and satisfied. Additionally, you can take them to bath to keep them clean and fresh, which contributes to their overall well-being. Beyond these basic needs, showing love and affection to your pets, such as playing with them or giving them attention, helps build a strong bond and keeps them content. These nurturing tasks not only enhance your pets' happiness but also contribute to your progress and earnings in the game, making the experience both enjoyable and rewarding.",
+    },
+    {
+      question: "How can I withdraw my Earnings",
+      answer:
+        "Players of Ace that Pays must meet certain criteria to be eligible to withdraw their earnings. Players must have at least 500 Php or 10 USD in their Game Wallet as the minimum withdrawal amount, with no limit for Gcash and  Gotyme. While Commision wallet follows a certain withdrawal bracket of a minimum of 500 Php up to 500,000 Php. Withdrawal requests can be made every Monday 12nn until 11:59pm and will be released the following Monday 12mn until 11:59am. When withdrawal is available, just go to the withdraw section of your dashboard and fill up the required information and click the request to proceed.",
+    },
+    {
+      question: "How do I create an Account",
+      answer:
+        "To get started with Ace that Pays, begin by visiting the official website at “www.AcethatPays.com” using your preferred web browser. This platform serves as the gateway to your gaming journey, where you can explore and engage with the various features offered. Once on the site, navigate to the social media links provided, which will direct you to Ace that Pays official social media pages or community groups. Here, you’ll need to connect with a “Team Leader” or an active member who can share a valid registration link with you. This link is crucial as it allows you to sign up and create your account. After receiving the link, proceed to register by filling in the required details, such as your name, email address, and any other necessary information. Completing this step successfully will grant you access to the platform, setting the stage for your adventure in the world of Ace that Pays.",
+    },
+    {
+      question: "What type of pets can I buy",
+      answer:
+        "In Ace that Pays, you have the opportunity to purchase a variety of pets from different trainers, each offering unique options to suit your preferences. These trainers are categorized into tiers such as Novice, Expert, Ace, Ace of Hearts, and Ace of Spade, each representing varying levels of expertise and rarity. The pets available range from adorable dogs and cats to colorful fishes, playful hamsters, and charming birds, ensuring there’s something for every animal lover. Whether you’re looking for a common companion or a rare and exclusive pet, these trainers provide a diverse selection to enhance your gameplay experience and help you build your ideal collection. This variety adds depth and excitement to the game, allowing you to choose pets that resonate with your style and strategy.",
+    },
+    {
+      question: "Can I have multiple accounts",
+      answer:
+        "In Ace that Pays, players are allowed to create and manage multiple accounts, providing flexibility to experiment with different strategies or maintain separate progress tracks. However, each account must be registered using a unique Gmail address. This means you can sign up for as many accounts as you like, as long as no two accounts share the same Gmail. This feature enables you to diversify your gameplay experience, whether you’re testing new approaches or organizing your activities across different accounts. It’s important, though, to adhere to the game’s rules and guidelines for each account to ensure fair play and avoid any potential issues. By following these guidelines, you can fully enjoy the benefits of having multiple accounts while staying within the game’s framework.",
+    },
+    {
+      question: "Can I report a bug or technical problem?",
+      answer:
+        "If you encounter a bug or technical issue while playing, you can contact us through any of our official social media accounts. When reaching out, please provide a clear and detailed description of the problem you’re facing. If available, include screenshots or any additional information that could help us better understand and resolve the issue. The more specific and comprehensive your report, the faster and more effectively our support team can assist you. Your feedback is invaluable in helping us enhance the game and create a smoother, more enjoyable experience for everyone.",
+    },
+    {
+      question: "How can I invite friends to play Ace that Pays?",
+      answer:
+        "You can invite friends to join Ace that Pays by sharing referral links directly from your dashboard. Your unique referral link will become available after you complete your first purchase on the platform. Simply send the link to your friends, and once they sign up using it, you can start earning rewards while expanding your network in the game.",
+    },
+]
+
--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,7 +1,20 @@
-import { Links } from '@/app/data'
-import React from 'react'
+'use client'
+import { Links, privacyPolicy, termsAndConditions } from '@/app/data'
+import React, { useState } from 'react'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
 
 export default function Footer() {
+  const [activeTab, setActiveTab] = useState("terms")
+
   return (
     <footer className=' w-full flex  items-center justify-center bg-zinc-900 '>
         <div className=' w-full  max-w-[1440px] flex flex-col gap-8 md:flex-row items-center justify-between py-6'>
@@ -11,11 +24,60 @@ export default function Footer() {
 
             <p className=" text-sm text-zinc-400">© 2025 All Rights Reserved Hashtag.</p>
 
-            <div className='flex items-center gap-4 text-sm'>
+            {/* <div className='flex items-center gap-4 text-sm'>
                 <a href="">Terms & Conditions</a>
                 <a href="">Privacy Policy</a>
     
-            </div>
+            </div> */}
+
+            <Dialog>
+            <DialogTrigger>
+              <div className=' flex text-xs items-center gap-4 mt-4 text-zinc-200'>
+                <p onClick={() => setActiveTab('terms')} className=' hover:text-yellow-500 cursor-pointer'>Terms & Conditions</p>
+                <p onClick={() => setActiveTab('privacy')} className=' hover:text-yellow-500 cursor-pointer'>Privacy Policy</p>
+
+              </div>
+            </DialogTrigger>
+            <DialogContent className=' bg-zinc-900 text-white w-[95%] md:w-full max-h-[90%] overflow-y-auto'>
+              <DialogHeader>
+                <DialogTitle>Hashtag Legal Information</DialogTitle>
+                <DialogDescription>
+                  Please review our Terms & Conditions and Privacy Policy
+                </DialogDescription>
+              </DialogHeader>
+              <div className="w-full max-w-4xl mx-auto text-sm">
+              
+             
+                  <Tabs value={activeTab} onValueChange={setActiveTab}>
+                    <TabsList className="grid w-full grid-cols-2 bg-zinc-700">
+                      <TabsTrigger value="terms">Terms & Conditions</TabsTrigger>
+                      <TabsTrigger value="privacy">Privacy Policy</TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="terms">
+                      <Accordion type="single" collapsible className="w-full">
+                        {termsAndConditions.map((item, index) => (
+                          <AccordionItem value={`item-${index}`} key={index}>
+                            <AccordionTrigger>{item.title}</AccordionTrigger>
+                            <AccordionContent className=' text-zinc-200 whitespace-pre-wrap'>{item.content}</AccordionContent>
+                          </AccordionItem>
+                        ))}
+                      </Accordion>
+                    </TabsContent>
+                    <TabsContent value="privacy">
+                      <Accordion type="single" collapsible className="w-full">
+                        {privacyPolicy.map((item, index) => (
+                          <AccordionItem value={`item-${index}`} key={index}>
+                            <AccordionTrigger>{item.title}</AccordionTrigger>
+                            <AccordionContent className=' text-zinc-200 whitespace-pre-wrap'>{item.content}</AccordionContent>
+                          </AccordionItem>
+                        ))}
+                      </Accordion>
+                    </TabsContent>
+                  </Tabs>
+               
+              </div>
+            </DialogContent>
+          </Dialog>
         </div>
            
     </footer>
